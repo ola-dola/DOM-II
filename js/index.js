91f9c606f4d815bf85fdcba8f9c9a54a96c2f910
oredola <oredolaolamide@gmail.com>
2019-10-29 15:50:45 +0100

    Added mouseover event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+let navClick = document.querySelector('header div');
+navClick.addEventListener('mouseover', (e) => {
+  e.target.style.color = 'blue';
+
+  setTimeout(() => {
+    e.target.style.color = "";
+  }, 500)
+});
