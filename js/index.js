b7db499b1fe402fc734ae274575f5f16372be672
oredola <oredolaolamide@gmail.com>
2019-10-29 20:50:52 +0100

    Added click and mouseout event listeners

--- js/index.js
+++ js/index.js
@@ -8,6 +8,11 @@ navClick.addEventListener('mouseover', (e) => {
   }, 500)
 });
 
+navClick.addEventListener('click', (e) => {
+  e.target.style.color = 'red';
+  e.preventDefault();
+});
+
 window.addEventListener('keydown', () => {
   confirm('Is it okay if we send your key logs to NSA?');
 });
@@ -20,4 +25,27 @@ window.addEventListener('load', () => {
     text = "Google cannot track you now!";
   }
   alert(text);
+});
+
+let buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(item => {
+  item.addEventListener('mouseover', (e) => {
+    e.target.textContent = "Onto the FUN BUS!";
+    e.target.style.color = "darkslategray";
+  })
+});
+
+buttons.forEach(item => {
+  item.addEventListener('mouseout', (e) => {
+    e.target.textContent = "Sign Me Up!";
+    e.target.style.color = "white";
+  })
+});
+
+buttons.forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.target.textContent = "Thank you!"
+    e.target.style.color = 'red';
+  })
 });
